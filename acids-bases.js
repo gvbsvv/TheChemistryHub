8b5e62d32133270bb4c0458c672576bdfbd6d60c
gvbsvv <gvbsvv@gmail.com>
2025-02-12 14:27:54 -0500

    Create acids-bases.js

---
+++ acids-bases.js
@@ -0,0 +1,12 @@
+import SearchResults from '../components/SearchResults';
+
+const AcidsBasesPage = () => {
+  return (
+    <div>
+      <h1>Acids and Bases</h1>
+      <SearchResults />
+    </div>
+  );
+};
+
+export default AcidsBasesPage;
